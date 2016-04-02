a48c2119eb7e98cd39d3a44fb4f86f3c243a53c9
dojineko <dojineko.spiral@gmail.com>
2016-04-02 23:22:15 +0900

    mainの処理を切り出した

---
+++ command.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+	"regexp"
+	"strconv"
+)
+
+func execute(query []string, path string) []Item {
+	var result []Item
+
+	var isFileSelect bool
+	if len(query) > 0 {
+		isFileSelect, _ = regexp.MatchString("^@.*?$", query[0])
+	}
+	if !isFileSelect || len(query) == 0 {
+		result = findAnyStorage(query, path)
+	} else if isFileSelect && len(query) == 1 {
+		result = findStorage(query, path)
+	} else {
+		filename := query[0][1:]
+		query = query[1:]
+		prefix := AlfredItemPrefix{
+			AutoComplete: "@" + filename,
+			Subtitle:     "Storage: " + filename,
+		}
+
+		records, err := loadCSV(path+"/"+filename, '\t')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if len(query) == 0 {
+			result = append(result, Item{
+				Title:    "キーワードを入力",
+				Subtitle: filename + "で検索するキーワードを指定してください",
+			})
+		} else {
+			linePattern := "^:([0-9]+?)$"
+			if m, _ := regexp.MatchString(linePattern, query[0]); m {
+				regexLine, _ := regexp.Compile(linePattern)
+				match := regexLine.FindStringSubmatch(query[0])[1]
+				matchInt, _ := strconv.Atoi(match)
+				prefix.Subtitle = prefix.Subtitle + ", RecordNo: " + match
+
+				result = findLine(query, records[matchInt], prefix)
+			} else {
+				result = findAny(query, records, prefix)
+			}
+		}
+	}
+
+	return result
+}
--- main.go
+++ main.go
@@ -1,61 +1,12 @@
 package main
 
-import (
-	"flag"
-	"log"
-	"regexp"
-	"strconv"
-)
+import "flag"
 
 func main() {
 	flag.Parse()
 	query := flag.Args()
+	path := "./storage"
 
-	storageDir := "./storage"
-
-	var result []Item
-	linePattern := "^:([0-9]+?)$"
-
-	var isFileSelect bool
-	if len(query) > 0 {
-		isFileSelect, _ = regexp.MatchString("^@.*?$", query[0])
-	}
-	if !isFileSelect || len(query) == 0 {
-		result = findAnyStorage(query, storageDir)
-	} else if isFileSelect && len(query) == 1 {
-		result = findStorage(query, storageDir)
-	} else {
-		filename := query[0][1:]
-		query = query[1:]
-		prefix := AlfredItemPrefix{
-			AutoComplete: "@" + filename,
-			Subtitle:     "Storage: " + filename,
-		}
-
-		records, err := loadCSV(storageDir+"/"+filename, '\t')
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if len(query) == 0 {
-			result = append(result, Item{
-				Title:    "キーワードを入力",
-				Subtitle: filename + "で検索するキーワードを指定してください",
-			})
-		} else {
-			if m, _ := regexp.MatchString(linePattern, query[0]); m {
-				regexLine, _ := regexp.Compile(linePattern)
-				match := regexLine.FindStringSubmatch(query[0])[1]
-				matchInt, _ := strconv.Atoi(match)
-				prefix.Subtitle = prefix.Subtitle + ", RecordNo: " + match
-
-				result = findLine(query, records[matchInt], prefix)
-			} else {
-				result = findAny(query, records, prefix)
-			}
-		}
-
-	}
-
+	result := execute(query, path)
 	printAlfred(result)
 }
