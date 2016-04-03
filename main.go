27e37d789844b3e06746fffb5d3b5aa433afc350
dojineko <dojineko.spiral@gmail.com>
2016-04-03 20:56:38 +0900

    オプションのパースをgo-flagsに変更

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
-import "flag"
+import (
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
+)
+
+// Options contains go-flags setting.
+type Options struct {
+}
+
+var opts Options
 
 func main() {
-	flag.Parse()
-	query := flag.Args()
+	parser := flags.NewParser(&opts, flags.Default)
+	args, _ := parser.Parse()
+	if len(args) == 0 {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(1)
+	}
+
+	query := args
 	path := "./storage"
 
 	result := execute(query, path)
